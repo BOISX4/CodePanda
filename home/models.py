cde7d15b70521405d6d56e8a9844c8c8e81677bb
Jeethendra Balaji <jeeboxical@gmail.com>
2021-04-18 00:00:47 +0530

    Added tags for ques

--- home/models.py
+++ home/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.utils import timezone
+from django.conf import settings
+from django.contrib.postgres.fields import ArrayField
 
 # Create your models here.
 class Question(models.Model):
@@ -7,16 +9,19 @@ class Question(models.Model):
     question = models.TextField()
     date_posted = models.DateTimeField(default=timezone.now)
     votes = models.IntegerField(default=0)
-    # user = models.ForeignKey(Users, on_delete=models.CASCADE)
+    user_ques = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, default=1)
+    tags_ques = ArrayField(models.CharField(max_length=30), blank=True, null=True)
 
     def __str__(self):
-        return self.question
+        return self.title
 
 class Answer(models.Model):
     answer = models.TextField()
     date_posted = models.DateTimeField(default=timezone.now)
     votes = models.IntegerField(default=0)
     answer_for_ques = models.ForeignKey(Question, on_delete=models.CASCADE)
+    user_ans = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, default=1)
+    # tags_answer = ArrayField(models.CharField(max_length=30), blank=True, null=True)
 
     def __str__(self):
         return self.answer
