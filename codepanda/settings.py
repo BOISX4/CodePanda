bf5027d553879ad98f1fa866464c1ea0c1b3d1d1
Karthik Srinivasan <56451150+EaguaireSama@users.noreply.github.com>
2021-04-18 01:40:17 +0530

    login and logout

--- codepanda/settings.py
+++ codepanda/settings.py
@@ -134,3 +134,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = 'home'
+LOGIN_URL = 'login'
