3b86c6d9568e6cfecd73ab25d1872ac2e4657474
dhanush07-star <gdhanush@am.students.amrita.edu>
2021-04-27 23:48:28 +0530

    minor changes

--- codepanda/urls.py
+++ codepanda/urls.py
@@ -24,11 +24,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', user_views.register, name = 'register'),
     path('profile/', user_views.profile, name = 'profile'),
-    path('login/', auth_views.LoginView.  as_view(template_name='users/login.html'), name = 'login'),
-    # path('password-reset/', auth_views.PasswordResetView.as_view(template_name='users/password_reset.html'), name = 'password_reset'),
-    # path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'), name = 'password_reset_done'),
-    # path('password-reset-confirm/<uidb64><token>', auth_views.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'), name = 'password_reset_confirm'),
-    
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name = 'login'),
     
     path('password-reset/',
          auth_views.PasswordResetView.as_view(
