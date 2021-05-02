66e76570c32f24823883bdd8d260b5f6e78acc6f
mhdSharuk <msharuk589@gmail.com>
2021-05-02 19:51:17 +0530

    commit

--- home/urls.py
+++ home/urls.py
@@ -1,10 +1,17 @@
 from django.urls import path
-from .views import QuestionListView , QuestionDetailView, QuestionCreateView, QuestionUpdateView, QuestionDeleteView
+from .views import (
+	QuestionListView,
+	QuestionDetailView,
+	QuestionCreateView,
+	QuestionUpdateView,
+	QuestionDeleteView,
+	UserPostListView )
 from . import views
 
 
 urlpatterns = [
     path('', QuestionListView.as_view(), name='home'),
+    path('user/<str:username>', UserPostListView.as_view(), name='user-posts'),
     path('question/<int:pk>/', QuestionDetailView.as_view(), name='question-detail'),
     path('question/<int:pk>/update/', QuestionUpdateView.as_view(), name='question-update'),
     path('question/<int:pk>/delete/', QuestionDeleteView.as_view(), name='question-delete'),
